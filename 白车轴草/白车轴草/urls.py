1e62c0785edf80fbc3544c77af8ed9d7581adfda
saitoasukakuku <saitoasukakuku@users.noreply.github.com>
2026-06-14 01:36:40 +0800

    Redirect root URL to index

--- 白车轴草/白车轴草/urls.py
+++ 白车轴草/白车轴草/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 from blog import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='index', permanent=False), name='home'),
     path('admin/', admin.site.urls),
     path('index/', views.index, name='index'),
     path('register/', views.register, name='register'),
