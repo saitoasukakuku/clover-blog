f7e590b9ddcbac34068832af2fe3a8c80e6e016d
saitoasukakuku <saitoasukakuku@users.noreply.github.com>
2026-06-13 23:41:03 +0800

    Add deployment basics

--- 白车轴草/白车轴草/settings.py
+++ 白车轴草/白车轴草/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i*iuulk6a4sg^$dmj-lg9@yn)$=w_&w(p-*abnzgkn$uj9*r)#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.20.100']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
