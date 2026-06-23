5ee631dc835e519da509d01961b060a512ca057c
saitoasukakuku <saitoasukakuku@users.noreply.github.com>
2026-06-23 21:48:02 +0800

    Register blog models in admin

--- 白车轴草/blog/admin.py
+++ 白车轴草/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post, UserProfile
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'status', 'created_at', 'updated_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('title', 'content', 'tags')
+    ordering = ('-created_at',)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'nickname', 'github_url', 'weibo_url', 'updated_at')
+    search_fields = ('user__username', 'nickname', 'bio')
+    ordering = ('user__username',)
